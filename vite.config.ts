896d54f651089af18c8764aa735208f747950dae
orenatopimenta <renato_p.oliveira@hotmail.com>
2025-05-30 17:41:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/virtual-move-coach/' : '/',
+  base: "/virtual-move-coach/",  // <-- essa linha é importante para Github Pages
   server: {
     host: "::",
     port: 8080,
