1aaf868726f2952ff9305d059ce7b1d1193f1d4d
orenatopimenta <renato_p.oliveira@hotmail.com>
2025-05-30 17:57:45 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const basename = '/virtual-move-coach/'
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename={basename}>
+    <App />
+  </BrowserRouter>
+)
