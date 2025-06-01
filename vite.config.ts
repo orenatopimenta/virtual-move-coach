0ef7c72c4d9177534541124a22c146313c0b5351
orenatopimenta <renato_p.oliveira@hotmail.com>
2025-06-01 16:11:59 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/virtual-move-coach/' : '/',
+  base: '/', // 👈 importante: "/" no Vercel!
   server: {
     host: "::",
     port: 8080,
